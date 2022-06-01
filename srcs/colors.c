d257d0ebffee566fbb1e67ae92eebb245ea26cf8
Thomas <tchalifour91@gmail.com>
2022-06-01 16:41:25 -0400

    end of day

--- srcs/colors.c
+++ srcs/colors.c
@@ -6,12 +6,16 @@
 /*   By: tchalifo <tchalifo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 14:38:21 by tchalifo          #+#    #+#             */
-/*   Updated: 2022/06/01 14:38:48 by tchalifo         ###   ########.fr       */
+/*   Updated: 2022/06/01 16:41:21 by tchalifo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
 
 int	get_t(int trgb)
 {
